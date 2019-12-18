ae54838bb03518428df6aad4dd340d245ba57457
xixzhang <59033342+xixzhang@users.noreply.github.com>
2019-12-18 18:36:27 -0500

    Delete main.js

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-	console.log(x)
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
